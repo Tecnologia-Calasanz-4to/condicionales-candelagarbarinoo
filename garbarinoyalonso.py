5bc7ef832b802cdec8378073894f4688477ecf02
candelagarbarinoo <c.garbarinorazzeto@colegiocalasanz.edu.ar>
2026-05-11 11:44:14 -0300

    Add files via upload

---
+++ garbarinoyalonso.py
@@ -0,0 +1,10 @@
+bisi = int(input("Decime un año"))
+if (bisi%4 == 0 and bisi%100 != 0) or bisi % 400 == 0:
+    print("El bisi "+str(bisi)+" es bisiesto")
+else: 
+    print("El bisi "+str(bisi)+" no lo es")
+
+
+
+    
+    
